cbe9db04887885485ed0b564960efbd0f2b7e650
antonmogul <139569942+antonmogul@users.noreply.github.com>
2024-04-11 11:33:27 +0000

    testing auto deploy on vercel

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 const DATA = [
   {
     id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
-    name: "Walmart",
+    name: "Category 1",
     items: [
       { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "3% Milk" },
       { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Butter" },
@@ -14,7 +14,7 @@ const DATA = [
   },
   {
     id: "487f68b4-1746-438c-920e-d67b7df46247",
-    name: "Indigo",
+    name: "Category 2",
     items: [
       {
         id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
@@ -26,7 +26,7 @@ const DATA = [
   },
   {
     id: "25daffdc-aae0-4d73-bd31-43f73101e7c0",
-    name: "Lowes",
+    name: "Category 3",
     items: [
       { id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc", name: "Workbench" },
       { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Hammer" },
